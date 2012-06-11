3a9daeade1e50e2affa5538e5f12ec36e87be7eb
Peter Horn <peter.horn@provideal.net>
2012-06-11 16:40:01 +0200

    better seeeds

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -1,7 +1,9 @@
-(1..14).each do |i|
-  MmGallery::ImageArtifact.create!(
-    title: "Image #{i}",
-    sid: "image#{i}",
-    file: File.new(File.join(File.dirname(__FILE__), 'images', "#{i}.png"))
-  )
+%w{Image Picture Graphic LoremIpsum}.each do |name|
+  (1..14).each do |i|
+    MmGallery::ImageArtifact.create!(
+      title: "#{name} #{i}",
+      sid: "#{name.parameterize}#{i}",
+      file: File.new(File.join(File.dirname(__FILE__), 'images', "#{i}.png"))
+    )
+  end
 end
